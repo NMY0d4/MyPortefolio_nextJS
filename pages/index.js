86816518dab70783dfa73bff2cd1f4e62f91c101
Grégory Marini <greg.marini@hotmail.fr>
2023-05-10 13:12:37 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import NavBar from '../components/NavBar';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Greg | Developper</title>
+        <meta
+          name='descritpion'
+          content="Grégory Marini, développeur FullStack,page d'accueil de mon site portefolio en nextJS"
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <NavBar />
+    </div>
+  );
+}
