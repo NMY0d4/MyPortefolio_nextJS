19122d829f2bbe612b88f3202557c392b490866b
Grégory Marini <greg.marini@hotmail.fr>
2023-05-16 18:41:30 +0200

    add signup with bcrypt

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,20 @@
-function handler() {
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+  const result = await db.collection('greg_portefolio').insertOne({
+    email,
+    password: hashPassword(password),
+  });
+
+  res.status(201).json({ message: 'Created user!' });
 }
 
 export default handler;
