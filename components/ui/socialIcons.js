4fbd05dde785ede8288c06ee4ffb4711cf4c5b63
Grégory Marini <greg.marini@hotmail.fr>
2023-05-20 17:44:03 +0200

    add last socials links

--- components/ui/socialIcons.js
+++ components/ui/socialIcons.js
@@ -1,12 +1,15 @@
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { AiOutlineMail } from 'react-icons/ai';
-import { BsFillPersonFill } from 'react-icons/bs';
+import { SiUdemy } from 'react-icons/si';
 
 export const socialIconClasses =
   'rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-125 ease-in duration-300';
 
 export const socialLinks = [
-  <a href='/'>
+  <a
+    href='https://www.linkedin.com/in/gr%C3%A9gory-marini-b71493136/'
+    target='_blank'
+  >
     <FaLinkedinIn />
   </a>,
   <a href='https://github.com/NMY0d4' target='_blank'>
@@ -15,7 +18,7 @@ export const socialLinks = [
   <a href='mailto:greg.marini@hotmail.fr'>
     <AiOutlineMail />
   </a>,
-  <a href='/#about'>
-    <BsFillPersonFill />
+  <a href='https://www.udemy.com/user/marini-gregory/' target='_blank'>
+    <SiUdemy />
   </a>,
 ];
