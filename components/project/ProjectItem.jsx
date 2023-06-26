f66aeeed2e864d8dbedf3ceeb8fea65807595ac1
Grégory Marini <greg.marini@hotmail.fr>
2023-06-26 10:54:30 +0200

    fix style responsive bug

--- components/project/ProjectItem.jsx
+++ components/project/ProjectItem.jsx
@@ -17,35 +17,39 @@ const ProjectItem = ({ project }) => {
 
   return (
     <div
-      className={`relative flex items-center justify-center h-[50vh] w-full shadow-xl shadow-gray-400 rounded-xl p-4 group transition duration-500 ${
+      className={`flex items-center justify-center h-[50vh] w-full shadow-xl shadow-gray-400 rounded-xl p-3 group transition duration-500 ${
         isHovered ? 'bg-gradient-to-r from-primary to-secondary' : 'bg-white'
       }`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
     >
-      <Image
-        className='rounded-xl'
-        layout='fill'
-        objectFit='cover'
-        objectPosition='center'
-        src={backgroundImg}
-        placeholder='blur'
-        alt={`$projet portefolio ${title}`}
-      />
       <div
-        className={`absolute bg-gradient-to-r p-4 rounded-xl from-primary to-secondary top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center transition-opacity duration-300 ${
-          isHovered ? 'opacity-100' : 'opacity-0'
-        }`}
+        className={`relative h-full w-full`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        <h3 className='text-2xl text-white tracking-wider'>{title}</h3>
-        <p className='pb-4 pt-2 text-white'>{technology} project</p>
-        <Link href={`/projects/${id}`}>
-          <p
-            className={`py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer ${scale110}`}
-          >
-            More Info
-          </p>
-        </Link>
+        <Image
+          className='rounded-xl'
+          layout='fill'
+          objectFit='cover'
+          objectPosition='center'
+          src={backgroundImg}
+          placeholder='blur'
+          alt={`$projet portefolio ${title}`}
+        />
+        <div
+          className={`absolute bg-gradient-to-r w-[40%] p-4 rounded-xl from-primary to-secondary top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center transition-opacity duration-300 ${
+            isHovered ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <h3 className='text-2xl text-white tracking-wider'>{title}</h3>
+          <p className='pb-4 pt-2 text-white'>{technology} project</p>
+          <Link href={`/projects/${id}`}>
+            <p
+              className={`py-3 w-[60%] mx-auto rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer ${scale110}`}
+            >
+              More Info
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
